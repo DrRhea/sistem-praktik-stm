df4b60db40dc9fa1de9cb4b6aa58bd8f5781ce17
safiraaliyah <166832611+safiraaliyah@users.noreply.github.com>
2024-08-10 23:39:28 +0700

    feat(admin/nav): ubah desain sidebar & navbar

--- resources/views/admin/components/navbar.blade.php
+++ resources/views/admin/components/navbar.blade.php
@@ -73,7 +73,7 @@
               <ul role="list" class="mt-2 -mx-2 space-y-1">
                 <li>
                   <a href="#" class="flex p-2 text-sm font-semibold leading-6 text-gray-400 rounded-md hover:text-white hover:bg-gray-800 group gap-x-3" x-state:on="Current" x-state:off="Default" x-state-description="Current: &quot;bg-gray-800 text-white&quot;, Default: &quot;text-gray-400 hover:text-white hover:bg-gray-800&quot;">
-                    <span class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">P</span>
+                    <span class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">p</span>
                     <span class="truncate">Pengajar</span>
                   </a>
                 </li>
